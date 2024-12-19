27e476eed6dacbc3ca1fda3171486daecc54ecf9
nitejkeshetty <nitejkeshetty@gmail.com>
2024-12-19 15:16:11 +0530

    first commit

---
+++ src/app/content/content.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './content.component.html',
+  styleUrl: './content.component.css',
+  encapsulation:ViewEncapsulation.None
+})
+export class ContentComponent {
+
+}
